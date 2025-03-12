005f829b003559eb074bd1572e1471a7c11a8245
Kevin Qian <kevinqian28@hotmail.com>
2025-03-12 10:47:22 -0600

    added more experience

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -21,10 +21,19 @@ const Experience = () => {
           <div className="timeline-start mb-10 md:text-end">
             <div className="text-lg font-black">Web Developer | Engineering Students’ Society</div>
             <div className="font-mono italic">Sep 2022 - Apr 2024</div>
-            <p className="text-start">
-              Drafted and developed modern user interfaces with JavaScript frameworks, HTML, and CSS, which increased audience engagement by 46%.
-              Collaborated with the IT team to enhance the ESS website by implementing new functionality and integrating a more streamlined design.
-            </p>
+            <div className="text-start">
+                <ul className="list-disc text-start text-white ml-5">
+                    <li>
+                        Drafted and developed modern user interfaces with JavaScript frameworks, HTML, and CSS, to improve outreach to engineers at the University of Alberta, ensuring better communication and engagement with students which increased audience engagement by 46%.
+                    </li>
+                    <li>
+                        Collaborated with the IT team to enhance the ESS website by implementing new functionality and integrating a more streamlined UI and UX design.
+                    </li>
+                    <li>
+                        Built filters to allow students to easily find companies hiring for their specific engineering discipline and see which companies were attending on each day. This improved the user experience and streamlined the job search process for students. 
+                    </li>
+                </ul>
+            </div>
           </div>
           <hr className="bg-primary" />
         </li>
@@ -46,12 +55,19 @@ const Experience = () => {
           <div className="timeline-end mb-10">
             <div className="text-lg font-black">React Developer | The Fresh Living</div>
             <div className="font-mono italic">May 2023 - Jun 2023</div>
-            <p>
-              Contracted to deliver a responsive, well-polished website within 2 months.
-              I leaveraged React, Next.js, and Tailwindcss to build modular components, improving code maintainability and consistency across the application.
-              With multilingual customers, implemented seamless multi-language support utilizing next-intl, enhancing user experience for international audiences.
-              Reduced webpage loading time and improved SEO performance using server rendered components.
-            </p>
+            <div>
+                <ul className="list-disc text-start text-white ml-5">
+                    <li>
+                        I leaveraged React, Node.js, Next.js and Tailwind CSS to build modular components, improving code maintainability and consistency across the application.
+                    </li>
+                    <li>
+                        With multilingual customers, implemented seamless multi-language support utilizing next-intl, enhancing user experience for international audiences.
+                    </li>
+                    <li>
+                        Reduced webpage loading time by 56% using Next.js server rendered components and improved SEO performance. 
+                    </li>
+                </ul>
+            </div>
           </div>
           <hr className="bg-primary" />
         </li>
@@ -73,20 +89,25 @@ const Experience = () => {
           <div className="timeline-start mb-10 md:text-end">
             <div className="text-lg font-black">Software Developer Student | PCL Construction</div>
             <div className="font-mono italic">Jan 2024 - Sep 2024</div>
-            <p className="text-start">
+            <div className="text-start">
                 <ul className="list-disc text-start text-white ml-5">
                     <li>
-                        Optimized the codebase and introduced new features for the Industrial Project Management (IPM) C# desktop application.
+                        Optimized the codebase and introduced new features for Industrial Project Management (IPM) C# desktop application, full-stack mobile kiosk web application using ASP.NET and Angular (TypeScript), and Enterprise Resource Planning C# desktop application.
                     </li>
                     <li>
                         Implemented a multi-threaded asynchronous report system in IPM, significantly improving efficiency by 5x in exporting batch reports for clients.
                     </li>
                     <li>
                         I wrote SQL queries to efficiently extract, transform, and visualize data, improving insights and decision-making.
-                        Also, developed predictive models based on historical in-shop work data to estimate future work capacity, enabling more informed, data-driven decisions.
                     </li>
                     <li>
-                        Implemented a microservice to dynamically load and display PDF specification drawings in Angular, supporting multiple file revisions and enabling annotations for enhanced collaboration.
+                        Developed predictive models in PowerBI based on historical in-shop work data to estimate future work capacity, enabling more informed, data-driven decisions.
+                    </li>
+                    <li>
+                        Refactored and optimized 1000+ lines of Angular frontend and backend code for improved performance and maintainability. Overhauled the UI/UX design based on feedback from users, to improve workflow.
+                    </li>
+                    <li>
+                        Implemented a microservice to dynamically load and display PDF technical drawings in Angular, supporting multiple file revisions and enabling annotations for enhanced collaboration.
                     </li>
                     <li>
                         Integrated automated testing into the CI/CD pipeline for an Angular application using Azure DevOps, improving deployment efficiency and software reliability.
@@ -95,7 +116,7 @@ const Experience = () => {
                         Engineered an automated ML pipeline using image processing and TensorFlow for data collection, preprocessing, and fine-tuning a ResNet model for accurate label recognition in technical drawings.
                     </li>
                 </ul>  
-            </p>
+            </div>
           </div>
           <hr className="bg-primary" />
         </li>
@@ -117,15 +138,27 @@ const Experience = () => {
           <div className="timeline-end mb-10">
             <div className="text-lg font-black">Undergraduate Research | University of Alberta</div>
             <div className="font-mono italic">Sep - Dec 2024</div>
-            <p>
-                Performed data analysis and visualization on the Cancer Cell Line Encyclopedia dataset using Pandas and Matplotlib.
-                I developed a machine learning pipeline using ML libraries from TensorFlow, PyTorch, scikit-learn to predict the effectiveness of different drug on certain
-                cancers using cancer genomic data. Performed data analysis and visualization on the Cancer Cell Line Encyclopedia dataset using Pandas and Matplotlib.
-                Preprocessing and training workflows were implemented using scikit-learn, streamlining data preparation 
-                and analysis. Dimensionality reduction techniques and various models, including Elastic Net, Graph Neural Networks (GNN), and Support Vector Machines
-                (SVM), were applied to optimize predictive performance. Validation helped identify the most effective approaches for improving
-                drug response predictions, contributing to more accurate and insightful genomic-driven treatment strategies. Supervised by PhD Dr. Lei Ma and wrote an academic paper.
-            </p>
+            <div>
+                <ul className="list-disc text-start text-white ml-5">
+                    <li>
+                        I developed a machine learning pipeline using ML libraries from TensorFlow, PyTorch, scikit-learn to predict the effectiveness of different drug on certain
+                        cancers using cancer genomic data. 
+                    </li>
+                    <li>
+                        Performed data analysis and visualization on the Cancer Cell Line Encyclopedia dataset using Pandas and Matplotlib.
+                    </li>
+                    <li>
+                        Implemented preprocessing and training workflows using scikit-learn for streamlined data preparation and cleaning.
+                    </li>
+                    <li>
+                        Evaluated and optimized predictive performance by applying dimensionality reduction techniques and diverse models,
+                        including Elastic Net, GNN, and SVM, to identify the most effective approaches for drug response predictions.
+                    </li>
+                    <li>
+                        Supervised by PhD Dr. Lei Ma and wrote an academic paper.
+                    </li>
+                </ul>
+            </div>
           </div>
           <hr className="bg-primary" />
         </li>
@@ -148,8 +181,8 @@ const Experience = () => {
             <div className="text-lg font-black">Available for 8-month co-op</div>
             <div className="font-mono italic">May 2025 - Dec 2025</div>
             <p>
-              As an 4th year student with 7 semesters completed, I'm looking to wrap my degree with experience in software engineering or machine learning.
-              I will return to school in January 2026 for my final semester, graduating May 2026.
+                As an 4th year student with 7 semesters completed, I'm looking to wrap my degree with experience in software engineering or machine learning.
+                I will return to school in January 2026 for my final semester, graduating May 2026.
             </p>
           </div>
           {/* <hr /> */}
