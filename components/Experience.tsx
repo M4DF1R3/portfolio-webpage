c80d4075c51984c8e26d4c09fa6e7a8e8ef13915
Kevin Qian <kevinqian28@hotmail.com>
2026-05-04 22:47:45 -0600

    updated experience

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -5,7 +5,7 @@ const Experience = () => {
     <div id="experience" className="bg-base-200 pb-8 px-24">
       <h1 className="header text-center font-bold text-6xl pb-8">Experience</h1>
       <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
-        <li>
+        {/* <li>
           <div className="timeline-middle">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -70,10 +70,9 @@ const Experience = () => {
             </div>
           </div>
           <hr className="bg-primary" />
-        </li>
+        </li> */}
 
         <li>
-          <hr className="bg-primary" />
           <div className="timeline-middle">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -87,15 +86,12 @@ const Experience = () => {
             </svg>
           </div>
           <div className="timeline-start mb-10 md:text-end">
-            <div className="text-lg font-black">Software Developer Student | PCL Construction</div>
+            <div className="text-lg font-black">Software Developer Co-op | PCL Construction</div>
             <div className="font-mono italic">Jan 2024 - Sep 2024</div>
             <div className="text-start">
                 <ul className="list-disc text-start text-white ml-5">
                     <li>
                         Optimized the codebase and introduced new features for Industrial Project Management (IPM) C# desktop application, full-stack mobile kiosk web application using ASP.NET and Angular (TypeScript), and Enterprise Resource Planning C# desktop application.
-                    </li>
-                    <li>
-                        Implemented a multi-threaded asynchronous report system in IPM, significantly improving efficiency by 5x in exporting batch reports for clients.
                     </li>
                     <li>
                         I wrote SQL queries to efficiently extract, transform, and visualize data, improving insights and decision-making.
@@ -136,7 +132,7 @@ const Experience = () => {
             </svg>
           </div>
           <div className="timeline-end mb-10">
-            <div className="text-lg font-black">ML Researcher | University of Alberta</div>
+            <div className="text-lg font-black">ML Researcher (Part-time) | University of Alberta</div>
             <div className="font-mono italic">Sep - Dec 2024</div>
             <div>
                 <ul className="list-disc text-start text-white ml-5">
@@ -178,7 +174,7 @@ const Experience = () => {
             </svg>
           </div>
           <div className="timeline-start mb-10 md:text-end">
-            <div className="text-lg font-black">Computer Vision | University of Alberta</div>
+            <div className="text-lg font-black">Computer Vision Researcher (Full-time)| University of Alberta</div>
             <div className="font-mono italic">May - Aug 2025</div>
             <div className="text-start">
                 <ul className="list-disc text-start text-white ml-5">
@@ -189,6 +185,43 @@ const Experience = () => {
                         Built image processing pipelines using OpenCV and scikit-image to analyze surface textures (GLCM, LBP) for reliable and robust interface detection in industrial chemical processes.
                     </li>
                 </ul>  
+            </div>
+          </div>
+          <hr className="bg-primary" />
+        </li>
+
+<li>
+          <hr className="bg-primary" />
+          <div className="timeline-middle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5">
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clipRule="evenodd" />
+            </svg>
+          </div>
+          <div className="timeline-end mb-10">
+            <div className="text-lg font-black">Software Developer Co-op  | PCL Construction</div>
+            <div className="font-mono italic">Sep - Dec 2024</div>
+            <div>
+                <ul className="list-disc text-start text-white ml-5">
+                    <li>
+                       Collaborated within an Agile environment, participating in daily stand-ups and sprint planning to implement an asynchronous multi-threaded reporting system using C# .NET, achieving a 5x performance boost in exporting client reports. 
+                    </li>
+                    <li>
+                        Championed Test-Driven Development (TDD) principles by writing comprehensive Unit tests and updating Playwright E2E test suites, ensuring high product quality and minimizing regression defects.
+                    </li>
+                    <li>
+                       Engineered a modular role-based access control system for a suite of WinForms applications by integrating an ASP.NET REST API service for centralized authentication and user profile retrieval.
+                    </li>
+                    <li>
+                        Improved corporate spending forecast accuracy by 13% by engineering interactive dashboards using Angular, HTML, CSS, and TypeScript to visualize complex financial data.
+                    </li>
+                </ul>
             </div>
           </div>
           <hr className="bg-primary" />
@@ -208,11 +241,11 @@ const Experience = () => {
                 clipRule="evenodd" />
             </svg>
           </div>
-          <div className="timeline-end mb-10 md:text-start">
-            <div className="text-lg font-black">Seeking Summer 2026 Internship or New Grad Positions</div>
-            <div className="font-mono italic">Starting May 2026</div>
+          <div className="timeline-start mb-10 md:text-end">
+            <div className="text-lg font-black">Seeking Fall 2026 Internship</div>
+            <div className="font-mono italic">Starting Sep 2026</div>
             <p>
-                I am actively seeking internship or new grad opportunities for Summer 2026 in software development, machine learning, or related fields. I can graduate in Spring 2026 or extend my degree to Spring 2027 if needed.
+                I am actively seeking internship or new grad opportunities for Fall 2026 in software development, machine learning, or related fields. I expect to graduate in Spring 2027.
             </p>
           </div>
           {/* <hr /> */}
